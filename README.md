326f5feca94afd2c887fb40a64996d752f8bf7d1
Vilson <khvilson@yandex.ru>
2023-06-16 07:11:25 +0300

    update

---
+++ README.md
@@ -0,0 +1,29 @@
+# 🎵 TriveVibe
+The TribeVibe website showcases talented musicians and organizes their live performances.
+
+The **Music Performance Agency Website** can be accessed via [this link](https://vilsonkh.github.io/tribe/).
+
+## Project Functionality
+The website provides information about the agency's services, musicians, and upcoming events. It uses animations and sliders to create an engaging experience.
+
+The sliders display musician profiles and past performances.
+
+The website is responsive, ensuring a seamless experience across devices.
+
+<img width="1920" alt="TribeVibe" src="">
+
+## Technology Stack
+The Theater project was built using the following technology stack:
+
+-   HTML;
+-   SASS;
+-   JQuery;
+- Bootstrap
+-   adaptive layout.
+
+## Installation
+To explore the website locally, you can clone the project using this command:
+
+```
+git clone https://github.com/vilsonkh/theatre
+```
--- js/main.js
+++ js/main.js
@@ -1,69 +1,98 @@
 $(function () {
-   $(".events__slider").owlCarousel({
-      rtl: true,
-      center: false,
-      items: 3,
-      loop: false,
-      margin: 15,
-      autoplay: false,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true,
-      nav: true,
-      navText: [
-         "<div class='nav-btn next-slide'><svg version='1.0' xmlns='http://www.w3.org/2000/svg' width='37.000000pt' height='31.000000pt' viewBox='0 0 37.000000 31.000000' preserveAspectRatio='xMidYMid meet'><g transform='translate(0.000000,31.000000) scale(0.100000,-0.100000)' fill='white' stroke='none'> <path d='M200 298 c0 -7 28 -38 63 -70 l62 -57 -162 -1 c-138 0 -163 -2 -163 -15 0 -13 25 -15 163 -15 l162 0 -65 -59 c-53 -48 -63 -62 -52 -72 10 -11 26 1 87 61 41 41 75 79 75 85 0 15 -143 155 -158 155 -6 0 -12 -5 -12 -12z'/></g></svg></div>",
-      ],
+	$(".events__slider").owlCarousel({
+		rtl: true,
+		center: false,
+		items: 3,
+		loop: false,
+		margin: 40,
+		autoplay: false,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		nav: true,
+		navText: [
+			"<div class='nav-btn next-slide'><svg version='1.0' xmlns='http://www.w3.org/2000/svg' width='37.000000pt' height='31.000000pt' viewBox='0 0 37.000000 31.000000' preserveAspectRatio='xMidYMid meet'><g transform='translate(0.000000,31.000000) scale(0.100000,-0.100000)' fill='white' stroke='none'> <path d='M200 298 c0 -7 28 -38 63 -70 l62 -57 -162 -1 c-138 0 -163 -2 -163 -15 0 -13 25 -15 163 -15 l162 0 -65 -59 c-53 -48 -63 -62 -52 -72 10 -11 26 1 87 61 41 41 75 79 75 85 0 15 -143 155 -158 155 -6 0 -12 -5 -12 -12z'/></g></svg></div>",
+		],
 
-      responsive: {
-         0: {
-            items: 1,
-         },
-         600: {
-            items: 1,
-         },
-         1000: {
-            items: 3,
-         },
-      },
-   });
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 3,
+			},
+		},
+	});
 
-   $(".artists__slider").owlCarousel({
-      items: 1,
-      slideSpeed: 2000,
-      autoplay: false,
-      dots: false,
-      loop: false,
-      responsiveRefreshRate: 200,
-   });
+	$("#singleArtistSlider").owlCarousel({
+		items: 1,
+		slideSpeed: 2000,
+		autoplay: false,
+		dots: false,
+		loop: false,
+		responsiveRefreshRate: 200,
+		margin: 50,
+		loop: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			800: {
+				items: 2,
+			},
+			1000: {
+				items: 3,
+			},
+			1100: {
+				items: 1,
+			},
+		},
+	});
 
-   $(".ever__slider").owlCarousel({
-      items: 3,
-      dots: false,
-      nav: true,
+	$(".ever__rightSlider").owlCarousel({
+		slideBy: "3",
+		items: 3,
+		dots: false,
+		loop: false,
+		responsiveRefreshRate: 200,
+		loop: true,
+		nav: true,
+		navText: [
+			"<div class='rightSlider__slider-prev nav-btn prev-slide'><svg version='1.0' xmlns='http://www.w3.org/2000/svg' width='37.000000pt' height='31.000000pt' viewBox='0 0 37.000000 31.000000' preserveAspectRatio='xMidYMid meet'><g transform='translate(0.000000,31.000000) scale(0.100000,-0.100000)' fill='white' stroke='none'><path d='M72 237 c-39 -40 -72 -77 -72 -82 0 -15 143 -155 158 -155 26 0 9 27 -50 82 l-63 57 163 1 c137 0 162 2 162 15 0 13 -25 15 -162 15 l-163 0 65 58 c40 36 61 63 56 70 -4 7 -10 12 -14 12 -4 0 -40 -33 -80 -73z'/> </g></svg></div> ",
+			"<div class='rightSlider__slider-next nav-btn next-slide'><svg version='1.0' xmlns='http://www.w3.org/2000/svg' width='37.000000pt' height='31.000000pt' viewBox='0 0 37.000000 31.000000' preserveAspectRatio='xMidYMid meet'><g transform='translate(0.000000,31.000000) scale(0.100000,-0.100000)' fill='white' stroke='none'> <path d='M200 298 c0 -7 28 -38 63 -70 l62 -57 -162 -1 c-138 0 -163 -2 -163 -15 0 -13 25 -15 163 -15 l162 0 -65 -59 c-53 -48 -63 -62 -52 -72 10 -11 26 1 87 61 41 41 75 79 75 85 0 15 -143 155 -158 155 -6 0 -12 -5 -12 -12z'/></g></svg></div>",
+		],
+	});
 
-      navText: [
-         "<div class='ever__slider-prev nav-btn prev-slide'><svg version='1.0' xmlns='http://www.w3.org/2000/svg' width='37.000000pt' height='31.000000pt' viewBox='0 0 37.000000 31.000000' preserveAspectRatio='xMidYMid meet'><g transform='translate(0.000000,31.000000) scale(0.100000,-0.100000)' fill='white' stroke='none'><path d='M72 237 c-39 -40 -72 -77 -72 -82 0 -15 143 -155 158 -155 26 0 9 27 -50 82 l-63 57 163 1 c137 0 162 2 162 15 0 13 -25 15 -162 15 l-163 0 65 58 c40 36 61 63 56 70 -4 7 -10 12 -14 12 -4 0 -40 -33 -80 -73z'/> </g></svg></div> ",
-         "<div class='ever__slider-next nav-btn next-slide'><svg version='1.0' xmlns='http://www.w3.org/2000/svg' width='37.000000pt' height='31.000000pt' viewBox='0 0 37.000000 31.000000' preserveAspectRatio='xMidYMid meet'><g transform='translate(0.000000,31.000000) scale(0.100000,-0.100000)' fill='white' stroke='none'> <path d='M200 298 c0 -7 28 -38 63 -70 l62 -57 -162 -1 c-138 0 -163 -2 -163 -15 0 -13 25 -15 163 -15 l162 0 -65 -59 c-53 -48 -63 -62 -52 -72 10 -11 26 1 87 61 41 41 75 79 75 85 0 15 -143 155 -158 155 -6 0 -12 -5 -12 -12z'/></g></svg></div>",
-      ],
+	$(".burger-button").on("click", () => {
+		$(".burger-menu").toggleClass("active");
+	});
 
-      smartSpeed: 200,
-      slideSpeed: 500,
-      slideBy: 3,
-      responsiveRefreshRate: 100,
-   });
+	$(".adv-slider").owlCarousel({
+		items: 1,
+		slideSpeed: 2000,
+		autoplay: false,
+		dots: true,
+		loop: false,
+		responsiveRefreshRate: 200,
+		margin: 50,
+		loop: true,
+	});
 });
 
 document.querySelector("body").addEventListener("mousemove", eyeball);
 
-function eyeball() {
-   const eye = document.querySelector(".iris");
+function eyeball(e) {
+	const eye = document.querySelector(".about__icon-iris");
 
-   let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
-   let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+	let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+	let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
 
-   let radian = Math.atan2(event.pageX - x, event.pageY - y);
-   let rotate = radian * (180 / Math.PI) * -1 + 270;
-   eye.style.transform = "rotate(" + rotate + "deg)";
-   eye.style.transformOrigin = "10px";
+	let radian = Math.atan2(e.pageX - x, e.pageY - y);
+	let rotate = radian * (180 / Math.PI) * 1 + 270;
+	eye.style.transform = "rotate(" + rotate + "deg)";
+	eye.style.transformOrigin = "10px";
 }
 
 let scrollPos = window.pageXOffset;
